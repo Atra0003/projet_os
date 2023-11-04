8077ce537366f7d602f2da94b82bea29e28faa85
Atra0003 <93745546+Atra0003@users.noreply.github.com>
2023-11-04 18:40:01 +0100

    Add files via upload
    avec sh

--- launcher.sh
+++ launcher.sh
@@ -2,51 +2,59 @@
 
 # Vérification du nombre d'arguments
 if [ $# -lt 2 ]; then
-    echo "Utilisation : $0 [-i | --interactive | -a | --automatic] chemin_image [autre_argument]"
+    # echo "Utilisation : $0 [-i | --interactive | -a | --automatic] chemin_image [autre_argument]"
+    echo "No similar image found (no comparison could be performed successfully)."
     exit 1
 fi
 
-# Vérification du chemin de l'image
-if ! [ -f "$2" ]; then
-    echo "Le paramètre 2 n'est pas un fichier image valide."
-    exit 2
-fi
 
-if [ "$1" == "-i" -o "$1" == "--interactive" ]; then # Mode interactif
+# Vérification du chemin de l'image
+if find img -type f -path "$2" -print | grep -q "$2"; then
+
+    if [ "$1" == "-i" -o "$1" == "--interactive" ]; then # Mode interactif
     bankImage=() # Création d'une banque d'images.
 
     while true; do
         read -p "Entrer le chemin d'une image (Ctrl+D pour quitter) : " input
 
-        if [ -z "$input" ]; then
-            echo -e "\nFin de la préparation de la banque d'images."
+        if [ -z "$input" ]; then # Fin de la préparation de la banque d'images.
             break
         fi
 
         # Vérification de la présence du fichier dans le dossier.
-        if [ -f "$input" ]; then
+        if find img -type f -path "$input" -print | grep -q "$input"; then
             echo "Le fichier est présent dans le dossier."
             bankImage+=("$input") # Ajout d'image dans la banque d'images.
         else
             echo "Le fichier n'est pas présent dans le dossier."
         fi
     done
-    # Appeler img-search.
-elif [ "$1" == "-a" -o "$1" == "--automatic" ]; then # Mode automatique
-    # Appeler img-search.
     make -B
 
-    # Vérifie si la compilation a réussi
-    if [ $? -eq 0 ]; then
-        echo "Compilation réussie."
-        ./img-search $2 $3
+    if [ $? -eq 0 ]; then # Compilation réussie.
+        ./img-search $2 $bankImage 
         # Exécute votre programme ou effectue d'autres actions si nécessaire
-    else
-        echo "Échec de la compilation."
     fi
-
+fi
 
 else
-    echo "Option non reconnue : $1"
-    exit 3
+    # echo "Le paramètre 2 (image) n'est pas une image du dossier image."
+    echo "No similar image found (no comparison could be performed successfully)."
+    #exit 1
+fi
+
+# === automatique === 
+
+
+if [ "$1" == "-a" -o "$1" == "--automatic" ]; then
+    if [ ! -d "$3" ]; then # Le paramètre 3 n'est pas un dossier 
+        exit 1
+    fi
+
+    make -B
+    #Vérifie si la compilation a réussi
+    if [ $? -eq 0 ]; then
+        ./img-search $2 $3 # Compilation réussie. 
+    fi
+
 fi
